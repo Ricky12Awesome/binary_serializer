a88bde5c460ad4850b6fca0970f9a1effee73c7d
Ricky12Awesome <rickygraveen@gmail.com>
2021-11-01 05:11:07 -0500

    Tuple and Unit struct support

--- binary_serializer/src/lib.rs
+++ binary_serializer/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use crate::common::*;
 use crate::decoder::*;
 use crate::encoder::*;
@@ -11,29 +13,4 @@ pub mod prelude {
   pub use crate::common::*;
   pub use crate::decoder::*;
   pub use crate::encoder::*;
-}
-
-#[derive(Debug, Eq, PartialEq, Serializer, Deserializer)]
-struct Data {
-  text: String,
-  number: u32,
-  list: Vec<u8>,
-}
-
-#[cfg(test)]
-#[test]
-fn run() {
-  let data = Data {
-    text: "Text".to_string(),
-    number: 69,
-    list: vec![4, 2, 0]
-  };
-
-  let bytes = data.to_bytes(ByteEndian::Little);
-  let data2 = Data::from_bytes(&bytes, ByteEndian::Little).unwrap();
-
-  println!("Serialized Bytes {:?}", &bytes);
-  println!("Original {:?}", data);
-  println!("Deserialized {:?}", data2);
-  assert_eq!(data, data2);
 }
--- binary_serializer_derive/src/lib.rs
+++ binary_serializer_derive/src/lib.rs
@@ -1,19 +1,63 @@
 use proc_macro::{self, TokenStream};
 use quote::quote;
-use syn::{parse_macro_input, Ident, DataUnion, DeriveInput, FieldsNamed, Field};
+use syn::{parse_macro_input, Ident, Path, Index, DeriveInput, FieldsNamed, FieldsUnnamed};
 
 mod serialize {
   use crate::*;
+
+  pub struct Paths {
+    serializer: Path,
+    encoder: Path,
+  }
+
+  impl Paths {
+    pub fn new() -> Self {
+      Self {
+        serializer: syn::parse_str("binary_serializer::encoder::Serializer").unwrap(),
+        encoder: syn::parse_str("binary_serializer::encoder::Encoder").unwrap(),
+      }
+    }
+  }
 
   pub(crate) fn struct_named(ident: Ident, fields: FieldsNamed) -> proc_macro2::TokenStream {
     let fields = fields.named.iter()
       .map(|f| &f.ident)
       .map(|name| quote! { encoder.encode_value(&self.#name) });
 
+    let Paths { serializer, encoder } = Paths::new();
+
     quote! {
-      impl Serializer for #ident {
-        fn encode(&self, encoder: &mut impl Encoder) {
+      impl #serializer for #ident {
+        fn encode(&self, encoder: &mut impl #encoder) {
           #(#fields);*
+        }
+      }
+    }
+  }
+
+  pub(crate) fn struct_unnamed(ident: Ident, fields: FieldsUnnamed) -> proc_macro2::TokenStream {
+    let fields = fields.unnamed.iter()
+      .enumerate()
+      .map(|(idx, _)| Index::from(idx))
+      .map(|idx| quote! { encoder.encode_value(&self.#idx) });
+
+    let Paths { serializer, encoder } = Paths::new();
+
+    quote! {
+      impl #serializer for #ident {
+        fn encode(&self, encoder: &mut impl #encoder) {
+          #(#fields);*
+        }
+      }
+    }
+  }
+
+  pub(crate) fn struct_unit(ident: Ident) -> proc_macro2::TokenStream {
+    let Paths { serializer, encoder } = Paths::new();
+
+    quote! {
+      impl #serializer for #ident {
+        fn encode(&self, encoder: &mut impl #encoder) {
         }
       }
     }
@@ -23,14 +67,32 @@ mod serialize {
 mod deserialize {
   use crate::*;
 
+  pub struct Paths {
+    deserializer: Path,
+    decoder: Path,
+    result: Path,
+  }
+
+  impl Paths {
+    pub fn new() -> Self {
+      Self {
+        deserializer: syn::parse_str("binary_serializer::decoder::Deserializer").unwrap(),
+        decoder: syn::parse_str("binary_serializer::decoder::Decoder").unwrap(),
+        result: syn::parse_str("binary_serializer::decoder::DecoderResult<Self>").unwrap(),
+      }
+    }
+  }
+
   pub(crate) fn struct_named(ident: Ident, fields: FieldsNamed) -> proc_macro2::TokenStream {
     let fields = fields.named.iter()
       .map(|f| &f.ident)
       .map(|name| quote! { #name: decoder.decode_value()? });
 
+    let Paths { deserializer, decoder, result } = Paths::new();
+
     quote! {
-      impl Deserializer for #ident {
-        fn decode(decoder: &mut impl Decoder) -> DecoderResult<Self>  {
+      impl #deserializer for #ident {
+        fn decode(decoder: &mut impl #decoder) -> #result {
           Ok(Self {
             #(#fields),*
           })
@@ -38,11 +100,40 @@ mod deserialize {
       }
     }
   }
+
+  pub(crate) fn struct_unnamed(ident: Ident, fields: FieldsUnnamed) -> proc_macro2::TokenStream {
+    let fields = fields.unnamed.iter()
+      .map(|_| quote! { decoder.decode_value()? });
+
+    let Paths { deserializer, decoder, result } = Paths::new();
+
+    quote! {
+      impl #deserializer for #ident {
+        fn decode(decoder: &mut impl #decoder) -> #result {
+          Ok(Self(#(#fields),*))
+        }
+      }
+    }
+  }
+
+  pub(crate) fn struct_unit(ident: Ident) -> proc_macro2::TokenStream {
+    let Paths { deserializer, decoder, result } = Paths::new();
+
+    quote! {
+      impl #deserializer for #ident {
+        fn decode(decoder: &mut impl #decoder) -> #result {
+          Ok(Self)
+        }
+      }
+    }
+  }
 }
 
 
-fn unimpl() -> proc_macro2::TokenStream {
-  quote! {}
+fn unimpl(_typ: &str) -> proc_macro2::TokenStream {
+  quote! {
+    compile_error!("Unimplemented: {}", _typ);
+  }
 }
 
 #[proc_macro_derive(Serializer)]
@@ -52,17 +143,12 @@ pub fn serialize(input: TokenStream) -> TokenStream {
   let output = match data {
     syn::Data::Struct(s) => match s.fields {
       syn::Fields::Named(fields) => serialize::struct_named(ident, fields),
-
-      syn::Fields::Unnamed(_) => unimpl(),
-      syn::Fields::Unit => unimpl(),
+      syn::Fields::Unnamed(fields) => serialize::struct_unnamed(ident, fields),
+      syn::Fields::Unit => serialize::struct_unit(ident),
     },
-    syn::Data::Enum(_) => unimpl(),
-    syn::Data::Union(
-      DataUnion {
-        fields: FieldsNamed { named, .. },
-        ..
-      }) => {
-      unimpl()
+    syn::Data::Enum(_) => unimpl("Enums"),
+    syn::Data::Union(_) => {
+      unimpl("Union?")
     }
   };
 
@@ -76,17 +162,12 @@ pub fn deserialize(input: TokenStream) -> TokenStream {
   let output = match data {
     syn::Data::Struct(s) => match s.fields {
       syn::Fields::Named(fields) => deserialize::struct_named(ident, fields),
-
-      syn::Fields::Unnamed(_) => unimpl(),
-      syn::Fields::Unit => unimpl(),
+      syn::Fields::Unnamed(fields) => deserialize::struct_unnamed(ident, fields),
+      syn::Fields::Unit => deserialize::struct_unit(ident),
     },
-    syn::Data::Enum(_) => unimpl(),
-    syn::Data::Union(
-      DataUnion {
-        fields: FieldsNamed { named, .. },
-        ..
-      }) => {
-      unimpl()
+    syn::Data::Enum(_) => unimpl("Enum"),
+    syn::Data::Union(_) => {
+      unimpl("Union?")
     }
   };
 
